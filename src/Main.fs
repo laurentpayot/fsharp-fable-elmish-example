8d6d02c5c307f6331eaab5e7a8f1dad0d98f9e3d
Laurent Payot <laurent.payot@publiberty.com>
2023-11-13 15:47:24 +0100

    React counter

--- src/Main.fs
+++ src/Main.fs
@@ -1,5 +1,33 @@
-﻿open Browser
+﻿open Fable.React
+open Fable.React.Props
+open Elmish
+open Elmish.React
 
-let div = document.createElement "div"
-div.innerHTML <- "Hello world!"
-document.body.appendChild div |> ignore
+
+type Model = { count: int }
+
+type Msg =
+    | Increment
+    | Decrement
+
+let init () : Model = { count = 0 }
+
+let update (msg: Msg) (model: Model) =
+    match msg with
+    | Increment -> { model with count = model.count + 1 }
+    | Decrement -> { model with count = model.count - 1 }
+
+
+let view model dispatch =
+    div
+        []
+        [ button [ OnClick(fun _ -> dispatch Decrement) ] [ str "-" ]
+          div [] [ str (sprintf "%A" model) ]
+          button [ OnClick(fun _ -> dispatch Increment) ] [ str "+" ] ]
+
+
+// App
+Program.mkSimple init update view
+|> Program.withReactBatched "root"
+// |> Program.withConsoleTrace
+|> Program.run
