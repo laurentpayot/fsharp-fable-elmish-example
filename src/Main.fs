03e79a9fb6a686b42548d06ee0ec6304fba31569
Laurent Payot <laurent.payot@publiberty.com>
2023-11-17 15:08:52 +0100

    Random button

--- src/Main.fs
+++ src/Main.fs
@@ -9,6 +9,7 @@ type Model = { count: int }
 type Msg =
     | Increment
     | Decrement
+    | Random
 
 let init () : Model = { count = 0 }
 
@@ -16,14 +17,20 @@ let update msg model =
     match msg with
     | Increment -> { model with count = model.count + 1 }
     | Decrement -> { model with count = model.count - 1 }
+    | Random ->
+        { model with
+            count = System.Random().Next(100) }
 
 
 let view model dispatch =
     div
         []
-        [ button [ OnClick(fun _ -> dispatch Decrement) ] [ str "-" ]
-          div [] [ str (sprintf "%A" model) ]
-          button [ OnClick(fun _ -> dispatch Increment) ] [ str "+" ] ]
+        [ p
+              []
+              [ button [ OnClick(fun _ -> dispatch Decrement) ] [ str "-" ]
+                str (sprintf "%A" model.count)
+                button [ OnClick(fun _ -> dispatch Increment) ] [ str "+" ] ]
+          p [] [ button [ OnClick(fun _ -> dispatch Random) ] [ str "Random" ] ] ]
 
 
 // App
