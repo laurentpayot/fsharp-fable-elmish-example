1474d89d6405a37e9644a07c79bbfd1e1291c7fb
Laurent Payot <laurent.payot@publiberty.com>
2023-11-29 15:04:21 +0100

    cat url string interpolation

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,11 @@ export function multiply(a, b) {
 }
 
 export async function catBase64(text, fontSize) {
-    const response = await fetch(`https://cataas.com/cat/says/${encodeURIComponent(text)}?filter=mono&fontColor=red&fontSize=${fontSize}&type=square`)
+    const response = await fetch(`https://cataas.com/cat/says/${
+        encodeURIComponent(text)
+    }?filter=mono&fontColor=red&fontSize=${
+        fontSize
+    }&type=square`)
     const array = await response.body?.getReader().read()
     return btoa(String.fromCharCode.apply(null, array?.value))
 }
