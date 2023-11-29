95008f9797a70d3150a3da276a1c0326edf2edd6
Laurent Payot <laurent.payot@publiberty.com>
2023-11-29 17:22:51 +0100

    JS lib in FFI folder

--- src/FFI/Lib.fs
+++ src/FFI/Lib.fs
@@ -5,6 +5,6 @@ open Fable.Core.JS
 open Fable.Core.JsInterop
 
 
-let multiply (a: int) (b: int) : int = importMember "../lib.js"
+let multiply (a: int) (b: int) : int = importMember "./lib.js"
 
-let catBase64 (text: string) (fontSize: int) : string Promise = importMember "../lib.js"
+let catBase64 (text: string) (fontSize: int) : string Promise = importMember "./lib.js"
