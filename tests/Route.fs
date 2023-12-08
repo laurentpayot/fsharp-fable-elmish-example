125d0d0082c0ad659d0301705557eaeb881aa89d
Laurent Payot <laurent.payot@publiberty.com>
2023-12-08 14:37:51 +0100

    organizing tests

--- tests/Route.fs
+++ tests/Route.fs
@@ -5,5 +5,11 @@ open Xunit
 open Route
 
 
-[<Fact>]
-let ``Home test`` () = Assert.True(toString Home = "/")
+type ``toString tests``() =
+
+    [<Fact>]
+    let ``Home`` () = Assert.True(toString Home = "/")
+
+    [<Fact>]
+    let ``Counter 0`` () =
+        Assert.True(toString (Counter 0) = "/counter/0")
