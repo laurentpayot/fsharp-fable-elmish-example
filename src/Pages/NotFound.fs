4e12b903ae76c35464aa83b5b4881cc42d9781c4
Laurent <laurent.payot@publiberty.com>
2024-09-04 12:21:11 +0200

    WIP: Feliz conversion: NotFound

--- src/Pages/NotFound.fs
+++ src/Pages/NotFound.fs
@@ -1,12 +1,13 @@
 module Pages.NotFound
 
-open Fable.React
-open Fable.React.Props
+open Feliz
+open type Html
 
+open View
 open Route
 
 let view: ReactElement list = [
-    h2 [] [ str "Page Not Found" ]
-    span [ Style [ FontSize "10rem" ] ] [ str "🕵️" ]
-    p [] [ linkTo Home [] [ str "Go Home" ] ]
+    h2 "Page Not Found"
+    span [ P.style [ S.fontSize (length.rem 10) ]; __' "🕵️" ]
+    p [ linkTo Home [ __' "Go Home" ] ]
 ]
