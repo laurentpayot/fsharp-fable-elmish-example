053e8e79253ce9fc1043458f3aafc957ad3d6e07
Laurent <laurent.payot@publiberty.com>
2024-09-07 11:20:54 +0200

    OnEvent fix to remove time duplicates

--- src/Shared/OnEvent.fs
+++ src/Shared/OnEvent.fs
@@ -4,19 +4,18 @@ open System
 open Fable.Core.JsInterop
 open Browser.Dom
 open Elmish
+open Browser.Types
 
 
 let onEvent (eventName: string) (toMsg: string -> 'msg) : ('msg Dispatch -> IDisposable) =
     let start dispatch =
-        document.addEventListener (
-            eventName,
-            (fun event -> dispatch (toMsg <| event?detail)),
-            false
-        )
+        let handler (event: Event) : unit = dispatch (toMsg <| event?detail)
+
+        document.addEventListener (eventName, handler, false)
 
         { new IDisposable with
             member _.Dispose() =
-                document.removeEventListener (eventName, (fun _ -> ()))
+                document.removeEventListener (eventName, handler, false)
         }
 
     start
